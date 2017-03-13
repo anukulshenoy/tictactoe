6920ace5ca5074b6751709324980677b2c68b58f
Anukul <shenoyanukul@gmail.com>
2017-03-13 12:07:25 -0700

    check winner and play functions for game

--- index.js
+++ index.js
@@ -1,29 +1,112 @@
+//node prompt for command line interaction
 var prompt = require('prompt');
-
 prompt.start();
 
-var board = [
-    [0,0,0],
-    [0,0,0],
-    [0,0,0]
-];
-console.log(board);
+//var player = "1";
 
-//var TictacToe = function () {
-//    var game = {};
-//    game.player1 = null;
-//    game.player2 = null;
-//    game.currentPlayer = null;
-//    game.board = [
-//        [0,0,0],
-//        [0,0,0],
-//        [0,0,0]
-//    ];
-//    game.checkWinner = function(board) {
-//        //check all rows
-//        for (var i = 0; i < board.length; i++) {
-//                var current = board[i][0];
-//            if (current === board[i][1] && current === board[i])
-//        }
-//    }
-//}
+//prompt.get(['Player ' + player + ' its your turn: '], function (err, result) {
+//    console.log(result);
+//});
+
+
+// game setup
+var TictacToe = function () {
+    var game = {};
+    game.player1 = "PLAYER 1";
+    game.player2 = "PLAYER 2";
+    game.winner = null;
+    game.currentPlayer = null;
+    //track the player position
+    game.gameover = false;
+    //create unique identifiers for each position on the board?
+    game.board = [
+        [1,1,1],
+        [1,1,1],
+        [1,1,1]
+    ];
+    game.renderBoard = function () {
+        //render the board in the command line in a user friendly form;
+            console.log(this.board[0] + '\n' + this.board[1] + '\n'  + this.board[2])
+
+    }
+    game.play = function () {
+        var currentPlayer = this.player2;
+        console.log(currentPlayer);
+        while(this.gamover === false) {
+            this.renderBoard();
+            if (currentPlayer === this.player1) {
+                currentPlayer = this.player2;
+            }
+            else {
+                currentPlayer = this.player1;
+            }
+            prompt.get(['Player ' + currentPlayer + ' its your turn: '], function (err, result) {
+                console.log(result);
+
+            })
+        }
+        this.gamover = true;
+    };
+    //see if we have a winner
+    game.checkWinner = function(board) {
+        //check all rows
+        for (var i = 0; i < board.length; i++) {
+            var current = board[i][0];
+            //check rows
+            if (current === board[i][1] && current === board[i][2]) {
+                if (current === 'X' || current === 'O') {
+                    this.gamover = true;
+                    if (current === 'X') {
+                        this.winner = this.player1;
+                    }
+                    else  {
+                        this.winner = this.player2;
+                    }
+                }
+
+            }
+            for (var j = 0; j < board[i].length; j++) {
+                var col = board[0][j];
+                if (col === board[1][j] && col === board[2][j]) {
+                    if (col === 'X' || col === 'O') {
+                        this.gamover = true;
+                        if (col === 'X') {
+                            this.winner = this.player1;
+                        }
+                        else  {
+                            this.winner = this.player2;
+                        }
+                    }
+                }
+            }
+        }
+        if (board[0][0] === board[1][1] && board[0][0] === board[2][2] && board[2][2] === board[1][1]) {
+            var col = board[0][0];
+            if (col === 'X' || col === 'O') {
+                this.gamover = true;
+                if (col === 'X') {
+                    this.winner = this.player1;
+                }
+                else  {
+                    this.winner = this.player2;
+                }
+            }
+        }
+        if (board[2][0] === board[1][1] && board[1][1] === board[0][2] && board[0][2] === board[2][0]) {
+            var col = board[1][1];
+            if (col === 'X' || col === 'O') {
+                this.gamover = true;
+                if (col === 'X') {
+                    this.winner = this.player1;
+                }
+                else  {
+                    this.winner = this.player2;
+                }
+            }
+        }
+    }
+    return game;
+}
+
+var tictactoe = TictacToe();
+tictactoe.play();
